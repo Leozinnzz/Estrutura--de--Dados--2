dc406d039bc1e3b825f96d74b63b9449844d1fb6
Leozinnzz <leozerathelegend@gmail.com>
2026-03-06 16:35:27 -0300

    Busca binaria resolvida

--- Bora_codar.c
+++ Bora_codar.c
@@ -1,24 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int busca(int alvo, int n, int arr[]) {
-	if(n == 0)
-		return -1;
-	if(alvo == arr[n-1])
-		return n-1;
-	else 
-		return busca(alvo, n-1, arr);
-	return -1;
+
+void ordenar(int arr[], int n) {
+	if(n <= 1)
+		return;
+	for(int i = 0; i < n-1; i++) {
+		if(arr[i] > arr[i+1]) {
+			int temp = arr[i];
+			arr[i] = arr[i+1];
+			arr[i+1] = temp;
+		}
+	}
 	
+	ordenar(arr, n-1);
+}
+int buscaBinaria(int alvo, int inicio, int fim, int arr[]){
+	if(inicio > fim)
+		return -1;
+	
+	int meio = (inicio + fim) / 2;
+	
+	if(arr[meio] == alvo)
+		return meio;
+		
+	if(alvo < arr[meio])
+		return buscaBinaria(alvo, inicio, meio -1,  arr);
+	else
+		return buscaBinaria(alvo, meio+1, fim, arr);
+	
+		
 }
 
 int main() {
-	
-	/*
-	 for(int i = 0; i < n; i++) {
-	 
-	}
-	 */
 	
 	int n;
     
@@ -32,12 +46,25 @@ int main() {
 		v[i] = rand()%100;
 		printf(" [%d] ", v[i]);
 	}
+
+	
+	ordenar(v, n);
+	
+	printf("\nVetor ordenardo\n");
+	
+	
+	for(int i = 0; i < n; i++) {
+		printf(" [%d] ", v[i]);
+	}
 	
 	int alv;
+	
 	printf("\nDigite o nuimero que deja buscar: ");
 	scanf("%d", &alv);
 	
-	int idc = busca(alv, n, v);
+	
+	int idc = buscaBinaria(alv, 0, n-1, v);
+	
 	if(idc == -1)
 		printf("Numero nao encontrado!");
 	else 
