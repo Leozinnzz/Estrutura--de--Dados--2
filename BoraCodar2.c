8274c1eb15e7e8fd333121cf73fae5314fe6d815
Leozinnzz <leozerathelegend@gmail.com>
2026-03-09 13:22:18 -0300

    Atividades concluidas

---
+++ BoraCodar2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int fibo(int p, int arr) {
+	if(p == 1)
+		return 0;
+	if(p == 2)
+		return 1;
+	return fibo(n-1) + fibo(n-2);
+}
+
+int main() {
+    
+    int n, resp; 
+    
+ 
+    printf("Digite um numero para a fibonacci: ");
+    scanf("%d", &n);
+    
+    
+
+    //the destroyer of codes
+}
+
+
--- Bora_codar.c
+++ Bora_codar.c
@@ -1,74 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int fiboR(int p){
+	if(p == 0)
+		return 0; 
+	if(p == 1)
+		return 1;
+	return fiboR(p-1) + fiboR(p-2);
+}
 
-void ordenar(int arr[], int n) {
-	if(n <= 1)
-		return;
-	for(int i = 0; i < n-1; i++) {
-		if(arr[i] > arr[i+1]) {
-			int temp = arr[i];
-			arr[i] = arr[i+1];
-			arr[i+1] = temp;
-		}
-	}
-	
-	ordenar(arr, n-1);
-}
-int buscaBinaria(int alvo, int inicio, int fim, int arr[]){
-	if(inicio > fim)
-		return -1;
-	
-	int meio = (inicio + fim) / 2;
-	
-	if(arr[meio] == alvo)
-		return meio;
-		
-	if(alvo < arr[meio])
-		return buscaBinaria(alvo, inicio, meio -1,  arr);
-	else
-		return buscaBinaria(alvo, meio+1, fim, arr);
-	
-		
-}
 
 int main() {
 	
+	int p;
+	
+	printf("\n FUNCAO RECURSIVA: \n");
+	
+	printf("Digite uma posição da sequencia de fibonacci: ");
+	scanf("%d", &p);
+	
+	int resp = fiboR(p);
+	
+	printf("O numero na posicao do termo e: %d", resp);
+	
+	printf("\n FUNCAO INTERATIVA: \n");
+	
 	int n;
-    
-    printf("Digite o tamanho do array: ");
-    scanf("%d", &n);
-    
-    int v[n];
-    
-    //preencher vetor
-    for(int i = 0; i < n; i++) {
-		v[i] = rand()%100;
-		printf(" [%d] ", v[i]);
+	printf("Digite a quantidade de termos: ");
+	scanf("%d", &n);
+	
+	int a = 0, b = 1, temp;
+	for(int i = 0; i < n; i++) {
+		printf(" %d ", a);
+		temp = a + b;
+		a = b; 
+		b = temp;
 	}
 
-	
-	ordenar(v, n);
-	
-	printf("\nVetor ordenardo\n");
-	
-	
-	for(int i = 0; i < n; i++) {
-		printf(" [%d] ", v[i]);
-	}
-	
-	int alv;
-	
-	printf("\nDigite o nuimero que deja buscar: ");
-	scanf("%d", &alv);
-	
-	
-	int idc = buscaBinaria(alv, 0, n-1, v);
-	
-	if(idc == -1)
-		printf("Numero nao encontrado!");
-	else 
-		printf("\nO numero foi encontrado no indice %d: ", idc);
 }
 
 
