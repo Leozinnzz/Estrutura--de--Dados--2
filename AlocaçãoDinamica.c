ee89eb77c25c5a460c8c88e73623ff2a6f7c8de9
Leozinnzz <leozerathelegend@gmail.com>
2026-04-06 10:43:23 -0300

    Exercicios treinos

--- AlocaçãoDinamica.c
+++ AlocaçãoDinamica.c
@@ -21,23 +21,32 @@ int main() {
 	
 	switch(interface()) {
 		case 1:
-			Pessoa* p1= new_Pessoa("Joao", 18, 1.60);
-			Pessoa* p2 = new_Pessoa("Leo", 20, 1.67);
-			Pessoa* p3 = new_Pessoa("Otavio", 18, 1.90);
+			Pessoa* p1 = criar_Pessoa("Leonardo", "1234506467", 19, 1.75);
+			Pessoa* p2 = criar_Pessoa("Joao", "8594302835", 22, 1.88);
+			Pessoa* p3 = criar_Pessoa("Matheus", "125643869", 32, 2.00);
+			Pessoa* p4 = criar_Pessoa("Victor", "7658345", 45, 1.74);
+			Pessoa* p5 = criar_Pessoa("Maria", "879544325", 52, 1.60);
+			Pessoa* p6 = criar_Pessoa("Ana", "76482467", 39, 1.72);
 			printf("\nPessoa criada\n");
 			break;
 		case 2:
 			Node* lista = NULL;
-			lista = new_node(lista, p1);
-			lista = new_node(lista, p2);
-			lista = new_node(lista, p3);
+			lista = criar_no(lista, p1);
+			lista = criar_no(lista, p2);
+			lista = criar_no(lista, p3);
+			lista = criar_no(lista, p4);
+			lista = criar_no(lista, p5);
+			lista = criar_no(lista, p6);
 			print_pessoa(lista);
 			break;
 		case 0:
-			delete_pessoa(p1);
-			delete_pessoa(p2);
-			delete_pessoa(p3);
-			delete_lista(lista);
+			deletar_pessoa(p1);
+			deletar_pessoa(p2);
+			deletar_pessoa(p3);
+			deletar_pessoa(p4);
+			deletar_pessoa(p5);
+			deletar_pessoa(p6);
+			deletar_lista(lista);
 			printf("\nAdeus\n");
 			return 0;
 		default:
--- Document.h
+++ Document.h
@@ -3,53 +3,58 @@
 #include <string.h>
 
 typedef struct {
-    char* nome;
-    int idade; 
-    float altura;
+	char* nome; 
+	char* cpf;
+	int idade;
+	float altura;
 }Pessoa;
 
-//builder
-Pessoa* new_Pessoa(char* nome, int idade, float altura) {
-    Pessoa* p = (Pessoa*) malloc(sizeof(Pessoa)); 
-    p->nome = (char*) malloc((strlen(nome) + 1 ));
-    strcpy(p->nome, nome);
-    p->idade = idade;
-    p->altura = altura;
-    return p;
-}
-//criacao da lista
 typedef struct Node {
-	Pessoa conteudo;
-	struct Node *next;
+	Pessoa content;
+	struct Node* next;
 }Node;
 
-//inserir na lista
-Node* new_node(Node* head, Pessoa* p){
-    Node* n = malloc(sizeof(Node)); 
-    n->conteudo = *p; 
-    n->next = head;
-    return n;
+//Construtor
+Pessoa* criar_Pessoa(char* nome, char* cpf, int idade, float altura){
+	Pessoa* p = malloc(sizeof(Pessoa));
+	p->nome = (char*) malloc(strlen(nome)+1);
+	strcpy(p->nome, nome);
+	p->cpf = (char*) malloc(strlen(cpf)+1);
+	strcpy(p->cpf, cpf);
+	p->idade = idade;
+	p->altura = altura;
+	return p;
 }
+
+//criar nos
+Node* criar_no(Node* head, Pessoa* content) {
+	Node* n = malloc(sizeof(Node)); 
+	n->content = *content;
+	n->next = head;
+	return n;
+}
+
 void print_pessoa(Node* lista){
-    while(lista != NULL){
-        printf("\nNome: %s\n", lista->conteudo.nome);
-        printf("Idade: %d\n", lista->conteudo.idade);
-        printf("Altura: %.2f\n", lista->conteudo.altura);
-        lista = lista->next;
-    }
-
-}
-void delete_pessoa(Pessoa* p){
-    if(p != NULL) {
-        free(p->nome);
-        free(p);
-    }
-}
-
-void delete_lista(Node* lista) {
 	while(lista != NULL) {
-		Node* proximo = lista->next;
-		free(lista);
-		lista = proximo;	
-	}	
+		printf("\nPessoa 1\n"); 
+		printf("Nome: %s\nCPF: %s\nIdade: %d\nAltura: %.2f\n", 
+		lista->content.nome, lista->content.cpf, lista->content.idade,
+		lista->content.altura);
+		lista = lista->next;
+	}
+}
+
+
+void deletar_pessoa(Pessoa* p) {
+	if(p == NULL) return;
+	free(p->nome);
+	free(p);
+}
+
+void deletar_lista(Node* list) {
+	while(list != NULL) {
+		Node* prox = list->next;
+		free(list);
+		list = prox;
+	}
 }
